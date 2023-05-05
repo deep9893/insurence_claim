f12da946955173d8a7453062f142a405d6c3d6f5
deep9893 <deeppratapsingh0996@gmail.com>
2023-05-05 19:57:44 +0530

    config & artifact file updated

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from insurance.logger import logging
 from insurance.exception import InsuranceException
 from insurance.utils import get_collection_as_dataframe
 import sys, os
-#from insurance.entity.config_entity import DataIngestionConfig
-#from insurance.entity import config_entity
+from insurance.entity.config_entity import DataIngestionConfig
+from insurance.entity import config_entity
 #from insurance.components.data_ingestion import DataIngestion
 #from insurance.components.data_validation import DataValidation
 
@@ -23,12 +23,12 @@ if __name__=="__main__":
      try:
           #start_training_pipeline()
           #test_logger_and_expection()
-        get_collection_as_dataframe(database_name ="INSURANCE", collection_name = 'INSURANCE_PROJECT')
-       #training_pipeline_config = config_entity.TrainingPipelineConfig()
+        #get_collection_as_dataframe(database_name ="INSURANCE", collection_name = 'INSURANCE_PROJECT')
+       training_pipeline_config = config_entity.TrainingPipelineConfig()
        
       #data ingestion
-       #data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-      # print(data_ingestion_config.to_dict())
+       data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+       print(data_ingestion_config.to_dict())
        #data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
       # data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
        
