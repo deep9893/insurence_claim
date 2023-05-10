364d1dc8c60e8c83e8e35d67c77ba69f8db9dfac
deep9893 <deeppratapsingh0996@gmail.com>
2023-05-10 18:28:28 +0530

    data validation

--- insurance/entity/artifact_entity.py
+++ insurance/entity/artifact_entity.py
@@ -6,9 +6,9 @@ class DataIngestionArtifact:
     train_file_path:str 
     test_file_path:str
 
-# @dataclass
-# class DataValidationArtifact:
-#     report_file_path:str
+@dataclass
+class DataValidationArtifact:
+    report_file_path:str
 
 # @dataclass
 # class DataTransformationArtifact:
--- insurance/utils.py
+++ insurance/utils.py
@@ -28,3 +28,50 @@ def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataF
         return df
     except Exception as e:
         raise InsuranceException(e, sys)
+    
+def write_yaml_file(file_path,data:dict):
+    try:
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(file_dir,exist_ok=True)
+        with open(file_path,"w") as file_writer:
+            yaml.dump(data,file_writer)
+    except Exception as e:
+        raise InsuranceException(e, sys)
+
+def convert_columns_float(df:pd.DataFrame,exclude_columns:list)->pd.DataFrame:
+    try:
+        for column in df.columns:
+            if column not in exclude_columns:
+                if df[column].dtypes != 'O':
+                    df[column]=df[column].astype('float')
+        return df
+    except Exception as e:
+        raise InsuranceException(e, sys)
+
+# def save_object(file_path: str, obj: object) -> None:
+#     try:
+#         os.makedirs(os.path.dirname(file_path), exist_ok=True)
+#         with open(file_path, "wb") as file_obj:
+#             dill.dump(obj, file_obj)
+#     except Exception as e:
+#         raise InsuranceException(e, sys) from e
+
+    
+# def load_object(file_path: str, ) -> object:
+#     try:
+#         if not os.path.exists(file_path):
+#             raise Exception(f"The file: {file_path} is not exists")
+#         with open(file_path, "rb") as file_obj:
+#             return dill.load(file_obj)
+#     except Exception as e:
+#         raise InsuranceException(e, sys) from e
+
+# def save_numpy_array_data(file_path: str, array: np.array):
+
+#     try:
+#         dir_path = os.path.dirname(file_path)
+#         os.makedirs(dir_path, exist_ok=True)
+#         with open(file_path, "wb") as file_obj:
+#             np.save(file_obj, array)
+#     except Exception as e:
+#         raise InsuranceException(e, sys) from e    
